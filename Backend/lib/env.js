408cf4c10c1aedf0a64e9bfe9633953e3e27844c
kithvin <kithvinbandara1994@gmail.com>
2025-12-07 08:57:23 +0530

    Synced authentication flow with Stream service

--- Backend/lib/env.js
+++ Backend/lib/env.js
@@ -20,4 +20,7 @@ export const ENV = {
   DB_URL: process.env.DB_URL,
   CLIENT_URL: process.env.CLIENT_URL,
   INNGEST_EVENT_KEY: process.env.INNGEST_EVENT_KEY,
+  INNGEST_SIGNING_KEY:process.env.INNGEST_SIGNING_KEY,
+  STREAM_API_KEY:process.env.STREAM_API_KEY,
+  STREAM_API_SECRET:process.env.STREAM_API_SECRET,
 };
--- Backend/lib/inngest.js
+++ Backend/lib/inngest.js
@@ -74,42 +74,89 @@
 //   }
 // );
 
+// import { Inngest } from "inngest";
+// import { connectDB } from "./db.js";
+// import User from "../models/User.js";
+
+// export const inngest = new Inngest({ id: "HireConnect" });
+
+// export const syncUser = inngest.createFunction(
+//   { id: "sync-user" },
+//   { event: "clerk/user.created" },
+//   async ({ event }) => {
+//     await connectDB();
+//     const { id, email_addresses, first_name, last_name, image_url } = event.data;
+
+//     await User.create({
+//       clerkid: id,
+//       email: email_addresses[0].email_address,
+//       name: `${first_name || ""} ${last_name || ""}`,
+//       profileImage: image_url,
+//     });
+
+//     console.log("🟢 User Created Successfully");
+//     return "User Created";
+//   }
+// );
+
+// export const deleteUserFromDB = inngest.createFunction(
+//   { id: "delete-user-from-db" },
+//   { event: "clerk/user.deleted" },
+//   async ({ event }) => {
+//     await connectDB();
+//     const { id } = event.data;
+
+//     await User.deleteOne({ clerkid: id });
+
+//     console.log("🔴 User Deleted Successfully");
+//     return "User Deleted";
+//   }
+// );
+
+// export const functions = [syncUser, deleteUserFromDB];
+
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import User from "../models/User.js";
+import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 
-export const inngest = new Inngest({ id: "HireConnect" });
+export const inngest = new Inngest({ id: "talent-iq" });
 
-export const syncUser = inngest.createFunction(
+const syncUser = inngest.createFunction(
   { id: "sync-user" },
   { event: "clerk/user.created" },
   async ({ event }) => {
     await connectDB();
+
     const { id, email_addresses, first_name, last_name, image_url } = event.data;
 
-    await User.create({
-      clerkid: id,
-      email: email_addresses[0].email_address,
+    const newUser = {
+      clerkId: id,
+      email: email_addresses[0]?.email_address,
       name: `${first_name || ""} ${last_name || ""}`,
       profileImage: image_url,
-    });
+    };
 
-    console.log("🟢 User Created Successfully");
-    return "User Created";
+    await User.create(newUser);
+
+    await upsertStreamUser({
+      id: newUser.clerkId.toString(),
+      name: newUser.name,
+      image: newUser.profileImage,
+    });
   }
 );
 
-export const deleteUserFromDB = inngest.createFunction(
+const deleteUserFromDB = inngest.createFunction(
   { id: "delete-user-from-db" },
   { event: "clerk/user.deleted" },
   async ({ event }) => {
     await connectDB();
+
     const { id } = event.data;
+    await User.deleteOne({ clerkId: id });
 
-    await User.deleteOne({ clerkid: id });
-
-    console.log("🔴 User Deleted Successfully");
-    return "User Deleted";
+    await deleteStreamUser(id.toString());
   }
 );
 
