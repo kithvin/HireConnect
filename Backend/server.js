13dfb2801e85b6e16302cdf72348896ce3004447
kithvin <kithvinbandara1994@gmail.com>
2025-12-21 10:44:05 +0530

    Synced authentication configuration with database settings

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,16 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
+import { serve } from "inngest/express";
 
 const app = express();
 
-console.log(ENV.PORT);
-console.log(ENV.DB_URL);
+// Middleware
+app.use(express.json());
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/", (req, res) => {
   res.status(200).json({ msg: "api is up and Backend" });
