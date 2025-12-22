0bcdfc500d7681b6becd1fd76933b9b9b598ed1f
kithvin <kithvinbandara1994@gmail.com>
2025-12-22 06:26:14 +0530

    Setup Tailwind, DaisyUI, TanStack Query, axios, lucide-react, react router dom & react hot toast

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,44 +1,42 @@
-import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/clerk-react";
+import { useUser } from "@clerk/clerk-react";
+import { Navigate, Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import { Toaster } from "react-hot-toast";
+import DashboardPage from "./pages/DashboardPage";
+import ProblemPage from "./pages/ProblemPage";
+import ProblemsPage from "./pages/ProblemsPage";
 
-export default function SignInPage() {
+function App() {
+  const { isSignedIn, isLoaded } = useUser();
+
+  
+  if (!isLoaded) return null;
+
   return (
-    <div className="min-h-screen flex items-center justify-center
-                    bg-gradient-to-br from-black via-slate-900 to-gray-900 px-4">
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />}
+        />
+        <Route
+          path="/dashboard"
+          element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />}
+        />
 
-      <div className="w-full max-w-md bg-gray-900/70 backdrop-blur
-                      border border-indigo-500/20 rounded-2xl
-                      shadow-2xl p-8 text-center">
+        <Route
+          path="/problem"
+          element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/problem/:id"
+          element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />}
+        />
+      </Routes>
 
-        <h1 className="text-3xl font-extrabold mb-2
-                       bg-gradient-to-r from-indigo-400 to-purple-500
-                       bg-clip-text text-transparent">
-          Welcome to HireConnect
-        </h1>
-
-        {/* USER NOT SIGNED IN */}
-        <SignedOut>
-          <p className="text-gray-400 text-sm mb-8">
-            Sign in to continue
-          </p>
-
-          <SignInButton mode="modal">
-            <button className="w-full py-3 rounded-xl
-                               bg-gradient-to-r from-indigo-500 to-purple-600
-                               text-white font-semibold">
-              Sign in
-            </button>
-          </SignInButton>
-        </SignedOut>
-
-        {/* USER ALREADY SIGNED IN */}
-        <SignedIn>
-          <p className="text-gray-400 mb-4">
-            You are already signed in
-          </p>
-          <UserButton afterSignOutUrl="/" />
-        </SignedIn>
-
-      </div>
-    </div>
+      <Toaster toastOptions={{ duration: 3000 }} />
+    </>
   );
 }
+
+export default App;
